aa381e40002b4d4fb908c93429870b26c7ba4806
Kasia Raczkier <katarzyna.raczkier@gmail.com>
2020-09-17 00:00:46 +0200

    Flash

--- blog/routes.py
+++ blog/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, flash
 from blog import app, db
 from blog.models import Entry
 from blog.forms import EntryForm
@@ -8,6 +8,7 @@ def homepage():
     posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
     return render_template("homepage.html", posts=posts)
 
+
 @app.route('/new-post', methods=["GET", "POST"])
 def create_entry():
     form = EntryForm()
@@ -16,7 +17,11 @@ def create_entry():
             new_post = Entry(title=form.data['title'], body=form.data['body'], is_published=form.data['is_published'])
             db.session.add(new_post)
             db.session.commit()
-        return redirect(url_for('create_entry()'))
+            if form.data['is_published']:
+                flash('New post added!')
+            else:
+                flash('Required box not checked. Post not added.')
+        return redirect(url_for('homepage'))
     return render_template("entry_form.html", form=form)
 
 if __name__ == "__main__":
